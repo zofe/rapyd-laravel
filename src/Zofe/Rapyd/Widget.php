045e2b242055007230b02864f6030b6286809163
zofe <felice.ostuni@gmail.com>
2013-12-21 22:53:24 +0100

    code format

--- src/Zofe/Rapyd/Widget.php
+++ src/Zofe/Rapyd/Widget.php
@@ -1,61 +1,61 @@
-<?php namespace Zofe\Rapyd;
+<?php
+
+namespace Zofe\Rapyd;
 
 class Widget
 {
 
-	public static $identifier = 0;
-	public $label = "";
-	public $output = "";
-	public $built = FALSE;
+    public static $identifier = 0;
+    public $label = "";
+    public $output = "";
+    public $built = FALSE;
     public $url;
-	
-	public function __construct()
-	{
+
+    public function __construct()
+    {
         $this->url = new Url();
-	}
+    }
 
-	/**
-	 * identifier is empty or a numeric value, it "identify" a single object instance.
+    /**
+     * identifier is empty or a numeric value, it "identify" a single object instance.
      * 
-	 * @return string identifier 
-	 */
-	protected function getIdentifier()
-	{
-		if (self::$identifier < 1)
-		{
-			self::$identifier++;
-			return "";
-		}
-		return (string) self::$identifier++;
-	}
+     * @return string identifier 
+     */
+    protected function getIdentifier()
+    {
+        if (self::$identifier < 1) {
+            self::$identifier++;
+            return "";
+        }
+        return (string) self::$identifier++;
+    }
 
-	/**
-	 * {placeholder} support for pesentation widgets
-	 * parse_pattern find all occurences of holders and return a simple array of matches
-	 * it's used for example to find "field" placeholders inside a datagrid column pattern
-	 * 
-	 * @param string $pattern
-	 * @return array of matches {placeholders} 
-	 */
-	public static function parse_pattern($pattern)
-	{
-		if (preg_match_all('/\{(\w+)\}/is', $pattern, $matches))
-		{
-			return $matches[1];
-		}
-	}
+    /**
+     * {placeholder} support for pesentation widgets
+     * parse_pattern find all occurences of holders and return a simple array of matches
+     * it's used for example to find "field" placeholders inside a datagrid column pattern
+     * 
+     * @param string $pattern
+     * @return array of matches {placeholders} 
+     */
+    public static function parse_pattern($pattern)
+    {
+        if (preg_match_all('/\{(\w+)\}/is', $pattern, $matches)) {
+            return $matches[1];
+        }
+    }
 
-	/**
-	 * "echo $widget" automatically call build() it and display $widget->output
-	 * however explicit build is preferred for a clean code
-	 * 
-	 * @return string 
-	 */
-	function __toString()
-	{
-		if ($this->output == "")
-			$this->build();
-		return $this->output;
-	}
+    /**
+     * "echo $widget" automatically call build() it and display $widget->output
+     * however explicit build is preferred for a clean code
+     * 
+     * @return string 
+     */
+    function __toString()
+    {
+        if ($this->output == "")
+            $this->build();
+        return $this->output;
+    }
 
 }
