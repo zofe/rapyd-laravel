9521f78b6c35ccf688a1fceb5ca6e9d5a22bae52
zofe <felice.ostuni@gmail.com>
2014-05-31 11:27:02 +0200

    column semplification

--- src/Zofe/Rapyd/DataGrid/Column.php
+++ src/Zofe/Rapyd/DataGrid/Column.php
@@ -1,29 +1,12 @@
 <?php namespace Zofe\Rapyd\DataGrid;
 
-use Zofe\Rapyd\Widget;
 
-class Column extends Widget
+class Column
 {
-
-    public $url = "";
-    
-    
     public $link = "";
-    public $linkRoute = "";
-    
     public $label = "";
-    public $attributes = array();
-    public $tr_attributes = array();
-    public $tr_attr = array();
     public $orderby = null;
-    public $orderby_asc_url;
-    public $orderby_desc_url;
-    protected $pattern = "";
-    protected $pattern_type = null;
-    protected $row_as = null;
-    protected $field = null;
-    protected $field_name = null;
-    protected $field_list = array();
+    public $attributes = array();
 
     public function __construct($name, $label = null, $orderby = false)
     {
@@ -43,7 +26,7 @@ class Column extends Widget
         return $this;
     }
 
-    public function link($url, $name = '', $position = 'BL', $attributes = array())
+    public function link($url)
     {
         $this->link = $url;
         return $this;
