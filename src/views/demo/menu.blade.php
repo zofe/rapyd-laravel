5e8859d7d324aba24b6bdf520709322b984f15f5
zofe <felice.ostuni@gmail.com>
2014-06-13 11:05:09 +0200

    menu

--- src/views/demo/menu.blade.php
+++ src/views/demo/menu.blade.php
@@ -10,8 +10,8 @@
     </div>
     <div class="collapse navbar-collapse main-collapse">
         <ul class="nav nav-tabs">
-            <li>{{ link_to("/", "Rapyd.com", 'target="_blank"') }}</li>
-            <li @if (Request::is('rapyd-demo')) class="active"@endif>{{ link_to("rapyd-demo", "Index") }}</li>
+            <li>{{ link_to("/", "Home", 'target="_blank"') }}</li>
+            <li @if (Request::is('rapyd-demo')) class="active"@endif>{{ link_to("rapyd-demo", "Demo Index") }}</li>
             <li @if (Request::is('rapyd-demo/models')) class="active"@endif>{{ link_to("rapyd-demo/models", "Models") }}</li>
             <li @if (Request::is('rapyd-demo/grid*')) class="active"@endif>{{ link_to("rapyd-demo/grid", "DataGrid") }}</li>
             <li @if (Request::is('rapyd-demo/filter*')) class="active"@endif>{{ link_to("rapyd-demo/filter", "DataFilter") }}</li>
