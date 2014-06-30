ff531d14a2905aeb772e6423cb5740cecd3f5606
zofe <felice.ostuni@gmail.com>
2014-06-30 21:03:08 +0200

    language strings updated

--- src/lang/en/rapyd.php
+++ src/lang/en/rapyd.php
@@ -1,9 +1,23 @@
 <?php
 
 return array(
-    'save' => 'Save',
-    'undo' => 'Undo',
-    'show' => 'Show',
-    'modify' => 'Modify',
-    'delete' => 'Delete',
+    'save'    => 'Save',
+    'undo'    => 'Undo',
+    'show'    => 'Show',
+    'modify'  => 'Modify',
+    'delete'  => 'Delete',
+    'add'     => 'Add',
+    'reset'   => 'Reset',
+    'search'  => 'Search',
+    'back'    => 'Back',
+
+    //dataedit
+    'de.inserted'   => 'New record created.',
+    'de.updated'    => 'The record has been updated.',
+    'de.deleted'    => 'The record has been deleted.',
+    'de.err'        => 'Error reading record.',
+    'de.err_unknown'=> 'Error, no record selected',
+    'de.err_dup_pk' => 'Error, duplicated primary key',
+    'de.conf_delete'=> 'Do you want to eliminate the record?',
+
 );
--- src/lang/it/rapyd.php
+++ src/lang/it/rapyd.php
@@ -1,9 +1,23 @@
 <?php
 
 return array(
-    'save' => 'Salva',
-    'undo' => 'Annulla',
-    'show' => 'Visualizza',
-    'modify' => 'Modifica',
-    'delete' => 'Elimina',
+    'save'    => 'Salva',
+    'undo'    => 'Annulla',
+    'show'    => 'Visualizza',
+    'modify'  => 'Modifica',
+    'delete'  => 'Elimina',
+    'add'     => 'Aggiungi',
+    'reset'   => 'Resetta',
+    'search'  => 'Cerca',
+    'back'    => 'Indietro',
+    
+    //dataedit
+    'de.inserted'   => 'Il record è stato inserito correttamente.',
+    'de.updated'    => 'Il record è stato modificato correttamente.',
+    'de.deleted'    => 'Il record è stato eliminato correttamente.',
+    'de.err'        => 'Si è verificato un errore',
+    'de.err_unknown'=> 'Si è verificato un errore, nessun record su cui operare.',
+    'de.err_dup_pk' => 'Si è verificato un errore, chiave primaria non univoca.',
+    'de.conf_delete'=> 'Vuoi davvero eliminare il record corrente?',
+
 );
