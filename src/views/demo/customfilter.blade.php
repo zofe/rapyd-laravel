cbf846ca3affe44420b2eae2185f652242ca3a2f
zofe <felice.ostuni@gmail.com>
2014-07-01 17:06:09 +0200

    datafilter some fashion filter link

--- src/views/demo/customfilter.blade.php
+++ src/views/demo/customfilter.blade.php
@@ -46,7 +46,7 @@
                 <div>
                     {{ $set->links() }}
                     <div class="pull-right">
-                        <a href="/rapyd-demo/customfilter">all atricles</a> |
+                        <a href="/rapyd-demo/customfilter">all articles</a> |
                         <a href="/rapyd-demo/customfilter?src=jhon+doe&search=1">jhon doe articles</a> |
                         <a href="/rapyd-demo/customfilter?src=jane+doe&search=1">jane doe articles</a>
                     </div>                    
