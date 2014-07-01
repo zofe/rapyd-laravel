c60345e29321e85fdb949596d032f9d3c17ef7af
zofe <felice.ostuni@gmail.com>
2014-07-01 16:13:10 +0200

    fix demo

--- src/controllers/DemoController.php
+++ src/controllers/DemoController.php
@@ -187,10 +187,9 @@ class DemoController extends \Controller {
             return $query->where('title','like','%'.$value.'%')
                         ->orWhere('body','like','%'.$value.'%')
                         ->orWhereHas('author', function($q) use($value) {
-                                            $q->where('firstname','like','%'.$value.'%')
-                                              ->orWhere('lastname','like','%'.$value.'%');
+                                       $q->whereRaw(" CONCAT(firstname, ' ', lastname) like ?", array("%".$value."%"));
                         })->orWhereHas('categories', function($q) use($value) {
-                                            $q->where('name','like','%'.$value.'%');
+                                       $q->where('name','like','%'.$value.'%');
                         });
         });
         $filter->submit('search');
