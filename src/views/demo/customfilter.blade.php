90b82d3ba0a5eddff6b5b5730223a6f9141b79d0
zofe <felice.ostuni@gmail.com>
2014-07-01 08:46:42 +0200

    custom filter+dataset styling demo

--- src/views/demo/customfilter.blade.php
+++ src/views/demo/customfilter.blade.php
@@ -34,10 +34,10 @@
                 <div class="row">
                     
                 @foreach ($set->data as $item)
-                    <div class="col-sm-4">
+                    <div class="col-sm-4" style="margin-bottom: 5px">
                         <strong>{{ $item->title }}</strong><br />
                         <em>{{ $item->author->firstname }} {{ $item->author->lastname }}</em><br />
-                        {{ implode(", ", $item->categories->lists("name"))  }}<br />
+                        <small>{{ implode(", ", $item->categories->lists("name"))  }}</small><br />
                     </div>
                 @endforeach
                     
