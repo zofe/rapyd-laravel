de89fab50dc65c78f7e8e25f70272bf793dc1419
zofe <felice.ostuni@gmail.com>
2014-07-25 23:30:54 +0200

    basic configuration file

--- src/Zofe/Rapyd/DataForm/Field/Field.php
+++ src/Zofe/Rapyd/DataForm/Field/Field.php
@@ -1,5 +1,6 @@
 <?php namespace Zofe\Rapyd\DataForm\Field;
 
+use Illuminate\Support\Facades\Config;
 use Zofe\Rapyd\Widget;
 use Zofe\Rapyd\Helpers\HTML;
 use Illuminate\Support\Facades\Form;
@@ -22,7 +23,7 @@ abstract class Field extends Widget
     public $rel_fq_other_key;
     public $rel_other_key;
 
-    public $attributes = array('class' => 'form-control');
+    public $attributes;
     public $output = "";
     public $visible = true;
     public $extra_output = "";
@@ -78,6 +79,7 @@ abstract class Field extends Widget
     {
         parent::__construct();
 
+        $this->attributes = Config::get('rapyd::field.attributes');
         $this->model = $model;
         $this->model_relations = $model_relations;
 
---
+++ src/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Config;
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Field Class
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+    'field'=> array(
+        'attributes' => array('class'=>'form-control'),
+    ),
+
+);
