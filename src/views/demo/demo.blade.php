ce15a5e98603861a6f80c5815578c161862c380b
zofe <felice.ostuni@gmail.com>
2014-07-30 07:41:47 +0200

    disqus for rapyd.com demo

--- src/views/demo/demo.blade.php
+++ src/views/demo/demo.blade.php
@@ -31,7 +31,41 @@
 
     @include('rapyd::demo.menu')
 
-    @yield('body')
+
+    <div class="row">
+
+        @if (isset($is_rapyd) AND $is_rapyd)
+
+            <div class="col-sm-9">
+                @yield('body')
+            </div>
+    
+            <div class="col-sm-3">
+                <div id="disqus_thread"></div>
+                <script type="text/javascript">
+                    var disqus_shortname = 'rapyd';
+                    (function() {
+                        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                    })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+            </div>
+
+        @else
+    
+            <div class="col-sm-12">
+                @yield('body')
+            </div>
+
+        @endif
+        
+    </div>
+
+
+
 
 @stop
 
