834bc954217567733e3476ee1b00837d0fc8e9fe
zofe <felice.ostuni@gmail.com>
2014-07-30 08:25:07 +0200

    demo  index  text

--- src/views/demo/demo.blade.php
+++ src/views/demo/demo.blade.php
@@ -21,6 +21,15 @@
             <br />
             {{ link_to('rapyd-demo/schema', "Populate Database", array("class"=>"btn btn-default")) }}
         @endif
+        
+        <br />
+        <br />
+        Click on tabs to see how rapyd widgets can save your time.<br />
+        The first tab <strong>Models</strong> is included just to show  models and relations used in this demo,
+        there isn't custom code, rapyd can work with your standard or extended Eloquent models.
+        <strong>DataSet</strong>, <strong>DataGrid</strong>, <strong>DataFilter</strong>, 
+        <strong>DataForm</strong>, and <strong>DataEdit</strong> are the "widgets" that rapyd provide.
+
     </p>
 
 
