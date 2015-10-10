fffec634226fc39d63acc37d2b30035170655c6a
zofe <felice.ostuni@gmail.com>
2015-10-10 16:50:55 +0200

    ability to return custom view on passed/saved closure in data-form|edit

--- src/DataForm/DataForm.php
+++ src/DataForm/DataForm.php
@@ -73,6 +73,7 @@ class DataForm extends Widget
     public $validator;
 
     public $output = "";
+    public $custom_output = null;
     public $fields = array();
     public $hash = "";
     public $error = "";
@@ -485,7 +486,10 @@ class DataForm extends Widget
             $result = $callable($this);
             if ($result && is_a($result, 'Illuminate\Http\RedirectResponse')) {
                 $this->redirect = $result;
+            } elseif ($result && is_a($result, 'Illuminate\View\View')) {
+                $this->custom_output = $result;
             }
+            
             //reprocess if an error is added in closure
             if ($this->process_status == 'error') {
                 $this->process();
@@ -548,6 +552,14 @@ class DataForm extends Widget
     }
 
     /**
+     * @return bool
+     */
+    public function hasCustomOutput()
+    {
+        return ($this->custom_output != null) ? true : false;
+    }
+    
+    /**
      * @return string
      */
     public function getRedirect()
@@ -570,7 +582,9 @@ class DataForm extends Widget
         if ($this->hasRedirect()) {
             return (is_a($this->redirect, 'Illuminate\Http\RedirectResponse')) ? $this->redirect : Redirect::to($this->redirect);
         }
-
+        if ($this->hasCustomOutput()) {
+            return $this->custom_output;
+        }
         return View::make($viewname, $array);
     }
 
