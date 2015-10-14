489c3d5f97321d411365dad9b2032f2d8b0f41fa
zhwei <zhangwei7@baixing.com>
2015-10-14 14:18:14 +0800

    save field rules to array
    + Laravel will explode rule to array
    
    https://github.com/laravel/framework/blob/5.1/src/Illuminate/Validation/
    Validator.php#L212
    
    + Support regex pattern with pipe character
    
    http://laravel.com/docs/5.1/validation#rule-regex

--- src/DataForm/Field/Field.php
+++ src/DataForm/Field/Field.php
@@ -76,7 +76,7 @@ abstract class Field extends Widget
         'null_label' => '[null]',
     );
 
-    public $rule = '';
+    public $rule = [];
 
     public $star = '';
     public $req = '';
@@ -157,10 +157,11 @@ abstract class Field extends Widget
      */
     public function rule($rule)
     {
-        $this->rule = trim($this->rule."|".$rule, "|");
-        if ((strpos($this->rule, "required") !== false) and !isset($this->no_star)) {
-            $this->required = true;
-        }
+	    $rule = is_string($rule) ? explode('|', $rule) : $rule;
+	    $this->rule = array_unique(array_merge($this->rule, $rule));
+	    if (in_array('required', $this->rule) and !isset($this->no_star)) {
+		    $this->required = true;
+	    }
 
         return $this;
     }
