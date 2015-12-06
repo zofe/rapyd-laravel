bec1242e87cb91acd5163cbf778852e586e564fc
Conrad Hellmann <conradhellmann@Die-Silberklinge.local>
2015-12-06 13:09:01 +0100

    Fix for not to include the (empty) toolbar html when not displaying a label or buttons.

--- views/toolbar.blade.php
+++ views/toolbar.blade.php
@@ -1,23 +1,30 @@
 
 
-<div class="btn-toolbar" role="toolbar">
 
-    @if (isset($label))
-    <div class="pull-left">
-        <h2>{!! $label !!}</h2>
-    </div>
-    @endif
-    @if (isset($buttons_left) && count($buttons_left))
-    <div class="pull-left">
-        @foreach ($buttons_left as $button) {!! $button !!}
-        @endforeach
-    </div>
-    @endif
-    @if (isset($buttons_right) && count($buttons_right))
-    <div class="pull-right">
-        @foreach ($buttons_right as $button) {!! $button !!}
-        @endforeach
-    </div>
-    @endif
-</div>
+@if ( 
+    (isset($label) && strlen($label)) || 
+    (isset($buttons_left) && count($buttons_left)) || 
+    (isset($buttons_right) && count($buttons_right))
+    )
+    <div class="btn-toolbar" role="toolbar">
 
+        @if (isset($label) && strlen($label))
+        <div class="pull-left">
+            <h2>{!! $label !!}</h2>
+        </div>
+        @endif
+        @if (isset($buttons_left) && count($buttons_left))
+        <div class="pull-left">
+            @foreach ($buttons_left as $button) {!! $button !!}
+            @endforeach
+        </div>
+        @endif
+        @if (isset($buttons_right) && count($buttons_right))
+        <div class="pull-right">
+            @foreach ($buttons_right as $button) {!! $button !!}
+            @endforeach
+        </div>
+        @endif
+    </div>
+     <br />
+@endif
