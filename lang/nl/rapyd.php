5c8c4502057724af74dd2388a1939d9a758b0e2b
Jeroen Coupé <jeroencoupe@gmail.com>
2016-01-25 14:05:18 +0100

    dutch translation files
    Added first version of dutch translartions

---
+++ lang/nl/rapyd.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+    'save'    => 'Opslaan',
+    'undo'    => 'Ongedaan maken',
+    'show'    => 'Toon',
+    'modify'  => 'Bewerk',
+    'delete'  => 'Verwijder',
+    'add'     => 'Toevoegen',
+    'reset'   => 'Reset',
+    'search'  => 'Zoek',
+    'back'    => 'Terug',
+
+    //dataedit
+    'inserted'   => 'Nieuw bestand toegevoegd.',
+    'updated'    => 'Het bestand is aangepast.',
+    'deleted'    => 'Het bestand is verwijderd.',
+    'err'        => 'Fout bij het lezen van het bestand.',
+    'err_unknown'=> 'Fout, er is geen bestand geselecteerd',
+    'err_dup_pk' => 'Fout, deze unieke id bestaat reeds',
+    'no_records' => 'Er is niets te tonen.',
+    'conf_delete'=> 'Wil je dit bestand verwijderen?',
+
+);
