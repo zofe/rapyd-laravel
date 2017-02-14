4573cbdd28029fd1be93fdcef26345af2c2ce58a
Mehmet Hanoğlu <mhanoglu55@gmail.com>
2017-02-14 15:25:13 +0200

    Turkish translation

---
+++ lang/tr/rapyd.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+    'save'    => 'Kaydet',
+    'undo'    => 'Geri al',
+    'show'    => 'Göster',
+    'modify'  => 'Düzenle',
+    'delete'  => 'Sil',
+    'add'     => 'Ekle',
+    'reset'   => 'Sıfırla',
+    'search'  => 'Ara',
+    'back'    => 'Geri',
+
+    //dataedit
+    'inserted'   => 'Yeni kayıt oluşturuldu.',
+    'updated'    => 'Kayıt güncellendi.',
+    'deleted'    => 'Kayıt silindi.',
+    'err'        => 'Kayıt okuma hatası.',
+    'err_unknown'=> 'Hata, kayıt seçilmedi',
+    'err_dup_pk' => 'Hata, primary key tekrarlandı',
+    'no_records' => 'Gösterilecek kayıt yok.',
+    'conf_delete'=> 'Bu kayıdı silmek istiyor musunuz?',
+
+);
