e9270bf2d11fbd56a631c5a4631b0d297979cba7
felice ostuni <felice.ostuni@axelero.it>
2018-07-23 16:21:51 +0200

    fix

--- src/routes.php
+++ src/routes.php
@@ -30,12 +30,12 @@ Route::group(['middleware' => 'web'], function () {
         Route::get('/set',          'DemoController@getSet');
         Route::get('/grid',         'DemoController@getGrid');
         Route::get('/filter',       'DemoController@getFilter');
-        Route::get('/customfilter','DemoController@getCustomfilter');
+        Route::get('/customfilter', 'DemoController@getCustomfilter');
         Route::any('/form',         'DemoController@anyForm');
-        Route::any('/advancedform','DemoController@anyAdvancedform');
-        Route::any('/styledform',  'DemoController@anyStyledform');
+        Route::any('/advancedform', 'DemoController@anyAdvancedform');
+        Route::any('/styledform',   'DemoController@anyStyledform');
         Route::any('/edit',         'DemoController@anyEdit');
-        Route::any('/data-tree',    'DemoController@anyDatatree');
+        Route::any('/datatree',     'DemoController@anyDatatree');
         Route::any('/menuedit',     'DemoController@anyMenuedit');
         Route::get('/nudegrid',     'DemoController@getNudegrid');
         Route::any('/nudeedit',     'DemoController@anyNudeedit');
